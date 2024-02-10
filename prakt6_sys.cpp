4919f6e6de75cf4d4848c65aa034ab23e2b83bfc
Rabotyaga1155 <123551706+Rabotyaga1155@users.noreply.github.com>
2024-02-10 10:07:39 +0500

    Add files via upload

---
+++ prakt6_sys.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <clocale>
+
+
+
+void findSpace(char* str);
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    char input[100];
+    printf("Введите строку символов: ");
+    fgets(input, sizeof(input), stdin);
+
+    findSpace(input);
+
+    return 0;
+}
+
+void findSpace(char* str) {
+    int removedSpaces = 0;
+    int len = strlen(str);
+
+    for (int i = 0; i < len; i++) {
+        if (str[i] == ' ') {
+            for (int j = i; j < len; j++) {
+                str[j] = str[j + 1];
+            }
+            removedSpaces++;
+            len--;
+            i--;
+        }
+    }
+
+    printf("Преобразованный текст: %s\n", str);
+    printf("Количество удаленных пробелов: %d\n", removedSpaces);
+}
